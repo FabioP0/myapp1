d2febc467187817529385c89a34593e692cd35b5
Fabio <fabio_poveda@hotmail.com>
2023-03-24 11:55:21 -0500

    Initial commit

---
+++ app/controllers/movies_controller.rb
@@ -0,0 +1,40 @@
+class MoviesController < ApplicationController
+    def index
+        @movies = Movie.all
+    end
+
+    def new
+        @movie = Movie.new
+    end
+
+    def show
+        @movie = Movie.find(params[:id])
+    end
+
+    def edit
+        @movie = Movie.find(params[:id])
+    end
+
+    def create
+        @movie = Movie.new(movie_params)
+        if @movie.save
+            redirect_to movie_path(@movie)
+        else
+            render :new
+        end
+    end
+
+    def update
+        @movie = Movie.find(params[:id])
+        if @movie.update(movie_params)
+            redirect_to movie_path(@movie)
+        else
+            render :edit
+        end
+    end
+
+    private
+        def movie_params
+            params.require(:movie).permit(:codigo, :titulo, :director, :genero)
+        end
+end
